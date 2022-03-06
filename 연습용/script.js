82289e80b9eaffc8c580ececc77333dc2e96c3df
Lucky-BE <byungeun96@gmail.com>
2022-03-06 21:49:23 +0900

    2022-03-05

---
+++ 연습용/script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    var a = 1;
+    if (a == 1) {
+        alert("a는 1");
+    }
+};
+
+function clickBtn() {
+    let btnName = document.getElementById("btn1").value;
+    alert("버튼이름은 " + btnName);
+}
